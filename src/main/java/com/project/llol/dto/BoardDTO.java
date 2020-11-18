b77306c00c24e5d9e2644281a1b91d72fc7f656d
jungsang-hyeok <tkdgur1798@gmail.com>
2020-11-18 11:00:13 +0900

    테이블에 작성일 칼럼 추가로 인한 게시글, 댓글 DTO 수정

--- src/main/java/com/project/llol/dto/BoardDTO.java
+++ src/main/java/com/project/llol/dto/BoardDTO.java
@@ -2,7 +2,14 @@ package com.project.llol.dto;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 public class BoardDTO {
-
+    private int boardnum;
+    private String boardtitle;
+    private String boardcontent;
+    private String boardimage;
+    private String boardyoutube;
+    private Timestamp boarddate;
 }
--- src/main/java/com/project/llol/dto/ReplyDTO.java
+++ src/main/java/com/project/llol/dto/ReplyDTO.java
@@ -2,9 +2,12 @@ package com.project.llol.dto;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 public class ReplyDTO {
     private int replynum;
     private int boardnum;
     private String replycontent;
+    private Timestamp replydate;
 }
