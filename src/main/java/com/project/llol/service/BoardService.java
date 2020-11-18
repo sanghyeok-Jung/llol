15bf5cb8851833cc388d75872f9e1e0d01252ab0
jungsang-hyeok <tkdgur1798@gmail.com>
2020-11-18 12:00:34 +0900

    게시판 서비스 클래스 구현

--- src/main/java/com/project/llol/service/BoardService.java
+++ src/main/java/com/project/llol/service/BoardService.java
@@ -1,7 +1,42 @@
 package com.project.llol.service;
 
+import com.project.llol.dao.BoardMapper;
+import com.project.llol.dto.BoardDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BoardService {
+    @Autowired
+    private BoardMapper mapper;
+
+    public int getBoardCount() {
+        return mapper.getBoardCount();
+    }
+
+    List<BoardDTO> getBoardList(int p_start, int p_end) {
+        return mapper.getBoardList(p_start, p_end);
+    }
+
+    BoardDTO getBoard(BoardDTO dto) {
+        return mapper.getBoard(dto);
+    }
+
+    void increaseVisitCount(BoardDTO dto) {
+        mapper.increaseVisitCount(dto);
+    }
+
+    void insertBoard(BoardDTO dto) {
+        mapper.insertBoard(dto);
+    }
+
+    void updateBoard(BoardDTO dto) {
+        mapper.updateBoard(dto);
+    }
+
+    void deleteBoard(BoardDTO dto) {
+        mapper.deleteBoard(dto);
+    }
 }
