cbc93b40405bec1201be0ceeb3c83e2a76103748
jungsang-hyeok <tkdgur1798@gmail.com>
2020-11-18 15:32:01 +0900

    - 페이징 처리 작업 진행 (현재 페이지 기준 좌/우로 3개의 페이지 번호 버튼이 출력되도록 함) - 시작 번호와 종료 번호 설정 후 html로 데이터 전달

--- src/main/java/com/project/llol/controller/BoardController.java
+++ src/main/java/com/project/llol/controller/BoardController.java
@@ -25,13 +25,14 @@ public class BoardController {
     public String boardList(Model model, @RequestParam(value = "pageNum", required = false, defaultValue = "1") int pageNum) {
         // 전체 게시글 개수 조회
         int boardCount = boardService.getBoardCount();
+        int totalPages = 1;
 
         List<BoardDTO> boardList = null;
 
         // 게시글이 1개 이상일 때에만 동작, 없으면 null 리턴
         if(boardCount >= 1) {
             // 최대 페이지 번호 설정
-            int pages = (boardCount % POST_PER_PAGES) == 0 ? (boardCount / POST_PER_PAGES) : ((boardCount / POST_PER_PAGES) + 1);
+            totalPages = (boardCount % POST_PER_PAGES) == 0 ? (boardCount / POST_PER_PAGES) : ((boardCount / POST_PER_PAGES) + 1);
 
             // 게시글 번호(boardnum)를 기준으로 p_start ~ p_end 사이의 게시글 범위 지정을 위한 값 설정
             int p_end = boardCount - ((pageNum - 1) * POST_PER_PAGES);
@@ -43,12 +44,20 @@ public class BoardController {
             }
 
             boardList = boardService.getBoardList(p_start, p_end);
-
-            model.addAttribute("pages", pages);
         }
 
+        // 전체 페이지 개수
+        model.addAttribute("totalPages", totalPages);
+        // 게시글 목록 데이터
         model.addAttribute("boardList", boardList);
+        // 현재 페이지 번호
         model.addAttribute("pageNum", pageNum);
+
+        // 화면에 보여질 최소 페이지 번호, 최대 페이지 번호 (범위를 초과하면 next, prev 버튼이 보이도록 하기 위해 설정)
+        int viewFirstPage = (pageNum - 3) > 1 ? (pageNum - 3) : 1;
+        int viewLastPage = (pageNum + 3) <= totalPages ? (pageNum + 3 : totalPages);
+        model.addAttribute("viewFirstPage", viewFirstPage);
+        model.addAttribute("viewLastPage", viewLastPage);
         return "boardList";
     }
 }
