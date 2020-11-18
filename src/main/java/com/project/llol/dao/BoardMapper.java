52dd7557495bc351e07c53eddf94377ba85be196
jungsang-hyeok <tkdgur1798@gmail.com>
2020-11-18 11:58:09 +0900

    게시판 매퍼 인터페이스 구현

--- src/main/java/com/project/llol/dao/BoardMapper.java
+++ src/main/java/com/project/llol/dao/BoardMapper.java
@@ -1,9 +1,19 @@
 package com.project.llol.dao;
 
+import com.project.llol.dto.BoardDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @Mapper
 public interface BoardMapper {
+    int getBoardCount();
+    List<BoardDTO> getBoardList(int p_start, int p_end);
+    BoardDTO getBoard(BoardDTO dto);
+    void increaseVisitCount(BoardDTO dto);
+    void insertBoard(BoardDTO dto);
+    void updateBoard(BoardDTO dto);
+    void deleteBoard(BoardDTO dto);
 }
