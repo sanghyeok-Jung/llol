6c18e100eaf8ea85d3cd6cf7f6e11a1a9282cb4d
jungsang-hyeok <tkdgur1798@gmail.com>
2020-11-18 11:54:31 +0900

    회원 매퍼 인터페이스에 불필요한 public 제거

--- src/main/java/com/project/llol/dao/MemberMapper.java
+++ src/main/java/com/project/llol/dao/MemberMapper.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Mapper
 public interface MemberMapper {
-    public MemberDTO getMember(MemberDTO dto);
-    public void insertMember(MemberDTO dto);
-    public int checkMember(MemberDTO dto);
-    public void updateMember(MemberDTO dto);
+    MemberDTO getMember(MemberDTO dto);
+    void insertMember(MemberDTO dto);
+    int checkMember(MemberDTO dto);
+    void updateMember(MemberDTO dto);
 }
