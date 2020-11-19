49e331f71c434de0b36854cb26171ef936c2060f
jungsang-hyeok <tkdgur1798@gmail.com>
2020-11-20 00:13:35 +0900

    주석 수정

--- src/main/java/com/project/llol/config/WebMvcConfig.java
+++ src/main/java/com/project/llol/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // URI가 '/upload/' 이하로 요청 될 경우, 로컬 disk의 특정 위치 경로로 파일을 사용자에게 제공하겠다 라는 뜻
+        // URI가 'upload/' 이하로 요청 될 경우, 로컬 disk의 특정 위치 경로로 파일을 사용자에게 제공하겠다 라는 뜻
         registry.addResourceHandler("upload/**").addResourceLocations("file://" + filePath);
     }
 }
